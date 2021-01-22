fdc4a247bc29fbb12443a794dd1ea34fb251779f
DustinPark92 <p4569zz@gmail.com>
2021-01-22 16:55:12 +0900

    Initial Commit

---
+++ SwiftUI02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI02
+//
+//  Created by Dustin on 2021/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
